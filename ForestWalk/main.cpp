472f428c8ae950ee2814d18f1c740ea180f266bf
Katherine456719 <alexandra.lung@s.unibuc.ro>
2023-12-22 22:37:14 +0200

    Add camera movement

--- ForestWalk/main.cpp
+++ ForestWalk/main.cpp
@@ -1,123 +1,345 @@
-// Codul sursa este adaptat dupa OpenGLBook.com
+﻿//	Biblioteci
+#include <iostream>
+#include <GL/glew.h>        //  Definește prototipurile functiilor OpenGL si constantele necesare pentru programarea OpenGL moderna; 
+#include <GL/freeglut.h>    //	Include functii pentru: 
+//	- gestionarea ferestrelor si evenimentelor de tastatura si mouse,
+//  - desenarea de primitive grafice precum dreptunghiuri, cercuri sau linii,
+//  - crearea de meniuri si submeniuri;
+#include "loadShaders.h"    //	Fisierul care face legatura intre program si shadere;
+// 	Fisierul care face legatura intre program si shadere;
+#include "glm/glm.hpp"        //	Bibloteci utilizate pentru transformari grafice;
+#include "glm/gtc/matrix_transform.hpp"
+#include "SOIL.h"
 
-#include <windows.h>  // biblioteci care urmeaza sa fie incluse
-#include <stdlib.h> // necesare pentru citirea shader-elor
-#include <stdio.h>
-#include <GL/glew.h> // glew apare inainte de freeglut
-#include <GL/freeglut.h> // nu trebuie uitat freeglut.h
-#include "loadShaders.h"
+#include "Camera/camera.h"
 
 
-//////////////////////////////////////
 
+//	Numarul de multiplicari 
+#define INSTANCE_COUNT 40
+
+//  Identificatorii obiectelor de tip OpenGL; 
 GLuint
-VaoId,
-VboId,
-ColorBufferId,
-ProgramId;
+        VboId,
+        VaoId,
+        VbPos,
+        VbCol,
+        VbModelMat,
+        EboId,
+        ColorBufferId,
+        ProgramId,
+        viewLocation,
+        projLocation,
+        codColLocation;
+GLint winWidth = 1200, winHeight = 900;
+//	Variabile catre matricile de transformare;
+glm::mat4 view, projection;
+//	Variabila ce determina schimbarea culorii pixelilor in shader;
+GLint codCol;
+//	Valaorea lui pi;
+float const PI = 3.141592;
+bool keys[256];
+//	Elemente pentru matricea de proiectie;
+float width = 800, height = 600, zNear = 1.f, fov = 90.f * PI / 180;
+float gametime = 0, delta_t = 0, last_time = 0;
+Camera camera;
 
-
-void CreateVBO(void)
-{
-	// varfurile 
-	GLfloat Vertices[] = {
-		0.5f,  0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f,  0.5f, 0.0f, 1.0f,
-		0.5f,  0.5f, 0.0f, 1.0f
-	};
-
-	// culorile, ca atribute ale varfurilor
-	GLfloat Colors[] = {
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	};
-
-	// se creeaza un buffer nou
-	glGenBuffers(1, &VboId);
-	// este setat ca buffer curent
-	glBindBuffer(GL_ARRAY_BUFFER, VboId);
-	// varfurile sunt "copiate" in bufferul curent
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-	// se creeaza / se leaga un VAO (Vertex Array Object) - util cand se utilizeaza mai multe VBO
-	glGenVertexArrays(1, &VaoId);
-	glBindVertexArray(VaoId);
-	// se activeaza lucrul cu atribute; atributul 0 = pozitie
-	glEnableVertexAttribArray(0);
-	// 
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-
-	// un nou buffer, pentru culoare
-	glGenBuffers(1, &ColorBufferId);
-	glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
-	// atributul 1 =  culoare
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
-}
-void DestroyVBO(void)
-{
-	glDisableVertexAttribArray(1);
-	glDisableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glDeleteBuffers(1, &ColorBufferId);
-	glDeleteBuffers(1, &VboId);
-
-	glBindVertexArray(0);
-	glDeleteVertexArrays(1, &VaoId);
+void ProcessNormalKeysUp(unsigned char key, int x, int y) {
+    keys[key] = false;
 }
 
-void CreateShaders(void)
-{
-	ProgramId = LoadShaders("example.vert", "example.frag");
-	glUseProgram(ProgramId);
-}
-void DestroyShaders(void)
-{
-	glDeleteProgram(ProgramId);
+void ProcessNormalKeysDown(unsigned char key, int x, int y) {
+    keys[key] = true;
 }
 
-void Initialize(void)
-{
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // culoarea de fond a ecranului
-	CreateVBO();
-	CreateShaders();
+void ProcessNormalKeys(unsigned char key, int x, int y) {
+    ProcessNormalKeysDown(key, x, y);
+    if (keys['w']) {
+        if (keys['d']) {
+            camera.ProcessKeyboard(RFORWARD, gametime);
+        } else if (keys['a']) {
+            camera.ProcessKeyboard(LFORWARD, gametime);
+        } else {
+            camera.ProcessKeyboard(FORWARD, gametime);
+        }
+    }
+    if (keys['s']) {
+        if (keys['d']) {
+            camera.ProcessKeyboard(RBACKWARD, gametime);
+        } else if (keys['a']) {
+            camera.ProcessKeyboard(LBACKWARD, gametime);
+        } else {
+            camera.ProcessKeyboard(BACKWARD, gametime);
+        }
+    }
+    if (keys['a']) {
+        if (keys['w']) {
+            camera.ProcessKeyboard(LFORWARD, gametime);
+        } else if (keys['s']) {
+            camera.ProcessKeyboard(LBACKWARD, gametime);
+        } else {
+            camera.ProcessKeyboard(LEFT, gametime);
+        }
+    }
+    if (keys['d']) {
+        if (keys['w']) {
+            camera.ProcessKeyboard(RFORWARD, gametime);
+        } else if (keys['s']) {
+            camera.ProcessKeyboard(RBACKWARD, gametime);
+        } else {
+            camera.ProcessKeyboard(RIGHT, gametime);
+        }
+    }
+    if (key == 27) {
+        glutLeaveMainLoop();
+    }
+    if (key == 'f') {
+        glutFullScreen();
+    }
 }
-void RenderFunction(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT);       
 
-	// Functiile de desenare
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDrawArrays(GL_TRIANGLES, 3, 3);
+float centerX = winWidth / 2, centerY = winHeight / 2;
 
-	glFlush();
-}
-void Cleanup(void)
-{
-	DestroyShaders();
-	DestroyVBO();
-}
+void UseMouse(int x, int y) {
 
-int main(int argc, char* argv[])
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(100, 100); // pozitia initiala a ferestrei
-	glutInitWindowSize(600, 600); //dimensiunile ferestrei
-	glutCreateWindow("Grafica pe calculator - primul exemplu"); // titlul ferestrei
-	glewInit(); // nu uitati de initializare glew; trebuie initializat inainte de a a initializa desenarea
-	Initialize();
-	glutDisplayFunc(RenderFunction);
-	glutCloseFunc(Cleanup);
-	glutMainLoop();
+    float xoffset = (float) x - centerX;
+    float yoffset = centerY - (float) y;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+
+    if (x != centerX || y != centerY)
+        glutWarpPointer(centerX, centerY);
+
 }
 
+
+//  Crearea si compilarea obiectelor de tip shader;
+//	Trebuie sa fie in acelasi director cu proiectul actual;
+//  Shaderul de varfuri / vertex shader - afecteaza geometria scenei;
+//  Shaderul de fragment / Fragment shader - afecteaza culoarea pixelilor;
+void CreateShaders() {
+    ProgramId = LoadShaders("shaders/example.vert", "shaders/example.frag");
+    glUseProgram(ProgramId);
+}
+
+//  Se initializeaza un vertex Buffer Object (VBO) pentru tranferul datelor spre memoria placii grafice (spre shadere);
+//  In acesta se stocheaza date despre varfuri (coordonate, culori, indici, texturare etc.);
+void CreateVBO() {
+    //	COORDONATELE varfurilor;
+    GLfloat Vertices[] =
+            {
+                    //baza
+                    -50.0f, -50.0f, -50.0f, 1.0f,
+                    50.0f, -50.0f, -50.0f, 1.0f,
+                    50.0f, 50.0f, -50.0f, 1.0f,
+                    -50.0f, 50.0f, -50.0f, 1.0f,
+                    //varful
+                    25.0f, 25.0f, 50.0f, 1.0f,
+            };
+
+    //	CULORILE instantelor;
+    //	Culorile sunt generate in functie de indexul de instatiere - fiecare cub va avea o singura culoare;
+    glm::vec4 Colors[INSTANCE_COUNT];
+    for (int i = 0; i < INSTANCE_COUNT; i++) {
+        float a = float(i) / 4.0f;
+        float b = float(i) / 5.0f;
+        float c = float(i) / 6.0f;
+        Colors[i][0] = 0.35f + 0.30f * (sinf(a + 2.0f) + 1.0f);
+        Colors[i][1] = 0.25f + 0.25f * (sinf(b + 3.0f) + 1.0f);
+        Colors[i][2] = 0.25f + 0.35f * (sinf(c + 4.0f) + 1.0f);
+        Colors[i][3] = 1.0f;
+    }
+
+    //  MATRICELE instantelor - se defineste tiparul de desenare al cuburilor prin ROTIREA si TRANSLATIA cubului initial (INSTANCED RENDERING);
+    glm::mat4 MatModel[INSTANCE_COUNT];
+    for (int i = 0; i < INSTANCE_COUNT; i++) {
+        MatModel[i] =
+                glm::translate(glm::mat4(1.0f), glm::vec3(200 * sin(2.f * i * 180 / PI),    //	Deplasarea pe OX;
+                                                          200 * cos(2.f * i * 180 / PI),    //	Deplasarea pe OY;
+                                                          100 *
+                                                          i))                                    //	Deplasarea pe OZ;
+                * glm::rotate(glm::mat4(1.0f), i * PI / 8, glm::vec3(i, 2 * i * i, i / 3));
+    }
+
+    //  Indicii pentru varfuri;
+    GLubyte Indices[] =
+            {
+                    0, 1, 2, 3,  //fata de jos
+                    //lateral
+                    0, 4, 1, 4, 2, 4, 3, 4, 0,
+                    //muchii
+                    4, 0, 1, 4, 2, 4, 3, 2, 1, 0, 3
+            };
+
+    //  Transmiterea datelor prin buffere;
+
+    //  Se creeaza / se leaga un VAO (Vertex Array Object) - util cand se utilizeaza mai multe VBO;
+    //  Generarea VAO si indexarea acestuia catre variabila VaoId;
+    glGenVertexArrays(1, &VaoId);
+    glBindVertexArray(VaoId);
+
+    //  Se creeaza un buffer pentru COORDONATE;
+    //  Generarea bufferului si indexarea acestuia catre variabila VbPos;
+    glGenBuffers(1, &VboId);
+    //  Setarea tipului de buffer - atributele varfurilor;
+    glBindBuffer(GL_ARRAY_BUFFER, VboId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    //	Se activeaza lucrul cu atribute;
+    //  Se asociaza atributul (0 = coordonate) pentru shader;
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid *) 0);
+
+    //  Se creeaza un buffer pentru CULORI;
+    glGenBuffers(1, &VbCol);
+    glBindBuffer(GL_ARRAY_BUFFER, VbCol);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+    //	Se activeaza lucrul cu atribute;
+    //  Se asociaza atributul (1 = culoare) pentru shader;
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), (GLvoid *) 0);
+    //	Rata cu care are loc distribuirea culorilor per instanta;
+    glVertexAttribDivisor(1, 1);
+
+    //  Se creeaza un buffer pentru MATRICEA DE POZITIE (instantierea cuburilor);
+    glGenBuffers(1, &VbModelMat);
+    glBindBuffer(GL_ARRAY_BUFFER, VbModelMat);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(MatModel), MatModel, GL_STATIC_DRAW);
+    //	Se activeaza lucrul cu atribute pentru fiecare coloana din fiecare matrice de instantiere;
+    //	Se incepe de la primul indice de atribut disponibil (2) si se creeaza pe rand cate unul pentru fiecare coloana (2+i, unde i = 0..3);
+    for (int i = 0; i < 4; i++) {
+        glEnableVertexAttribArray(2 + i);
+        glVertexAttribPointer(2 + i,              //  Location;
+                              4, GL_FLOAT, GL_FALSE,                //  vec4;
+                              sizeof(glm::mat4),                    //  Stride;
+                              (void *) (sizeof(glm::vec4) * i));      //  Start offset;
+        glVertexAttribDivisor(2 + i, 1);
+    }
+
+    //	Se creeaza un buffer pentru INDICI;
+    glGenBuffers(1, &EboId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EboId);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
+// Elimina obiectele de tip shader dupa rulare;
+void DestroyShaders() {
+    glDeleteProgram(ProgramId);
+}
+
+void DestroyVBO() {
+    //  Eliberarea atributelor din shadere (pozitie, culoare, texturare etc.);
+    glDisableVertexAttribArray(2);
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(0);
+
+    //  Stergerea bufferelor pentru VARFURI (Coordonate, Culori, Instanitere), INDICI;
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glDeleteBuffers(1, &VboId);
+    glDeleteBuffers(1, &VbCol);
+    glDeleteBuffers(1, &VbModelMat);
+    glDeleteBuffers(1, &EboId);
+
+    //  Eliberaea obiectelor de tip VAO;
+    glBindVertexArray(0);
+    glDeleteVertexArrays(1, &VaoId);
+}
+
+
+//  Functia de eliberare a resurselor alocate de program;
+void Cleanup() {
+    DestroyShaders();
+    DestroyVBO();
+}
+
+//  Setarea parametrilor necesari pentru fereastra de vizualizare;
+void Initialize() {
+    glClearColor(1.f, 1.0f, 1.0f, 0.0f);        //  Culoarea de fond a ecranului;
+    CreateVBO();                                //  Trecerea datelor de randare spre bufferul folosit de shadere;
+    CreateShaders();                            //  Initilizarea shaderelor;
+    //	Instantierea variabilelor uniforme pentru a "comunica" cu shaderele;
+    viewLocation = glGetUniformLocation(ProgramId, "view");
+    projLocation = glGetUniformLocation(ProgramId, "projection");
+    codColLocation = glGetUniformLocation(ProgramId, "codCol");
+}
+
+void UpdateTime() {
+    float time = glutGet(GLUT_ELAPSED_TIME) * 0.001f;
+    delta_t = time - last_time;
+    last_time = time;
+    gametime += delta_t;
+}
+
+//	Functia de desenare a graficii pe ecran;
+void RenderFunction() {
+    //  Se curata ecranul OpenGL pentru a fi desenat noul continut (bufferul de culoare & adancime);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    UpdateTime();
+    glBindVertexArray(VaoId);
+    glBindBuffer(GL_ARRAY_BUFFER, VboId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EboId);
+
+    // Matricea de vizualizare;
+    view = camera.GetViewMatrix();
+    glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
+
+    //	Realizarea proiectiei;
+    projection = glm::infinitePerspective(GLfloat(fov), GLfloat(width) / GLfloat(height), zNear);
+    glUniformMatrix4fv(projLocation, 1, GL_FALSE, &projection[0][0]);
+
+    //	Desenarea fetelor;
+    codCol = 0;                                                            //  Culoarea;
+    //	Transmiterea variabilei uniforme pentru COLORARE spre shadere;
+    glUniform1i(codColLocation, codCol);
+    //	Functia glDrawElementsInstanced primeste 4 argumente:
+    //	 - arg1 = modul de desenare;
+    //	 - arg2 = numarul de varfuri;
+    //	 - arg3 = tipul de date al indicilor;
+    //	 - arg4 = pointer spre indici (EBO): pozitia de start a indicilor;
+    //	 - arg5 = numarul de instante;
+
+    glDrawElementsInstanced(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_BYTE, (void *) (0), INSTANCE_COUNT);
+    glDrawElementsInstanced(GL_TRIANGLE_STRIP, 9, GL_UNSIGNED_BYTE, (void *) (4), INSTANCE_COUNT);
+    //  Desenarea muchiilor;
+    codCol = 1;                                         //	Se schimba culoarea;
+    glUniform1i(codColLocation, codCol);
+    glLineWidth(3.5);
+    glDrawElementsInstanced(GL_LINE_LOOP, 11, GL_UNSIGNED_BYTE, (void *) (13), INSTANCE_COUNT);
+
+    glutSwapBuffers();    //	Inlocuieste imaginea deseneata in fereastra cu cea randata;
+    glFlush();            //  Asigura rularea tuturor comenzilor OpenGL apelate anterior;
+}
+
+//	Punctul de intrare in program, se ruleaza rutina OpenGL;
+int main(int argc, char *argv[]) {
+    //  Se initializeaza GLUT si contextul OpenGL si se configureaza fereastra si modul de afisare;
+
+    glutInit(&argc, argv);
+    //	Se folosesc 2 buffere pentru desen (unul pentru afisare si unul pentru randare => animatii cursive) si culori RGB + 1 buffer pentru adancime;
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(winWidth, winHeight);                        //  Dimensiunea ferestrei;
+    glutInitWindowPosition(100, 100);                                //  Pozitia initiala a ferestrei;
+    glutCreateWindow(
+            "Instanced rendering - OpenGL <<nou>>");        //	Creeaza fereastra de vizualizare, indicand numele acesteia;
+
+    //	Se initializeaza GLEW si se verifica suportul de extensii OpenGL modern disponibile pe sistemul gazda;
+    //  Trebuie initializat inainte de desenare;
+
+    glewInit();
+
+    Initialize();                            //  Setarea parametrilor necesari pentru fereastra de vizualizare;
+    glutDisplayFunc(RenderFunction);        //  Desenarea scenei in fereastra;
+    glutIdleFunc(RenderFunction);            //	Asigura rularea continua a randarii;
+    glutPassiveMotionFunc(UseMouse);
+    glutKeyboardUpFunc(ProcessNormalKeysUp);
+    glutKeyboardFunc(ProcessNormalKeys);    //	Functii ce proceseaza inputul de la tastatura utilizatorului;
+    //glutSpecialFunc(ProcessSpecialKeys);
+    glutCloseFunc(Cleanup);                    //  Eliberarea resurselor alocate de program;
+    //  Bucla principala de procesare a evenimentelor GLUT (functiile care incep cu glut: glutInit etc.) este pornita;
+    //  Prelucreaza evenimentele si deseneaza fereastra OpenGL pana cand utilizatorul o inchide;
+
+    glutMainLoop();
+
+    return 0;
+}
